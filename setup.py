c52577bd3ad8fee381ae785342e6470eeee659c3
kadirpekel <kadirpekel@gmail.com>
2012-08-03 02:26:01 +0300

    bumped version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name = 'wrest',
     packages = ['wrest'],
-    version = '0.0.1',
+    version = '0.0.2',
     description = 'Thin rest client with no dependencies',
     author='Kadir Pekel',
     author_email='kadirpekel@gmail.com',
