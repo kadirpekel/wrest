cef72af6ce7fdc16fb6bf813666370fa8d259517
kadirpekel <kadirpekel@gmail.com>
2012-08-02 17:47:17 +0300

    restructed project tree

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+ 
+setup(
+    name = 'wrest',
+    packages = ['wrest'],
+    version = '0.0.1',
+    description = 'Thin rest client with no dependencies',
+    author='Kadir Pekel',
+    author_email='kadirpekel@gmail.com',
+    url='https://github.com/kadirpekel/wrest',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Operating System :: OS Independent',
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: GIS'
+    ]
+)
