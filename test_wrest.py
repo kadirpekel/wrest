b2c6586c3265c8cca86af8d6f4670e0432ed4ce0
kadirpekel <kadirpekel@gmail.com>
2012-08-03 17:08:07 +0300

    replaced bottle with pure builtin wsgi server for test fixtures

--- test_wrest.py
+++ test_wrest.py
@@ -1,37 +1,47 @@
 import unittest
-from multiprocessing import Process
-from bottle import route, run, request
-from wrest import Client
 import time
+import json
+
+from multiprocessing import Process
+from wsgiref.simple_server import make_server
+
+from wrest import Client
 
 HOST = 'localhost'
 PORT = 8000
 BASE_URL = "http://%s:%s" % (HOST, PORT)
 PATH = '/sample/path/to/resource'
 
-@route(PATH, method=['GET', 'POST', 'PUT', 'DELETE'])
-def echo():
-    return {
-        'method': request.method,
-        'path': request.path,
-        'body': request.body.read(),
-        'headers': dict(request.headers),
-        'querystring': dict(request.query)
+def fixture_app(environ, start_response):
+    start_response('200 OK', [('Content-type','application/json')])
+    content_length = 0
+    try:
+        content_length = int(environ.get('CONTENT_LENGTH'))
+    except:
+        pass
+    headers = dict([(k, v) for k, v in environ.items() if k.find("HTTP_") == 0])
+    response_obj = {
+        'method': environ.get('REQUEST_METHOD'),
+        'path': environ.get('PATH_INFO'),
+        'body': environ.get('wsgi.input').read(content_length),
+        'headers': headers,
+        'querystring': environ.get('QUERY_STRING')
     }
+    return json.dumps(response_obj)
 
 class TestCaseWrest(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.server = Process(target=run, kwargs={'host': HOST, 'port': PORT,
-                                                                'quiet': True})
-        cls.server.start()
+        cls.server = make_server(HOST, PORT, fixture_app)
+        cls.server_proc = Process(target=cls.server.serve_forever)
+        cls.server_proc.start()
         time.sleep(1) # Let server start in parallel execution
         cls.client = Client(BASE_URL)
-    
+
     @classmethod
     def tearDownClass(cls):
-        cls.server.terminate()
+        cls.server_proc.terminate()
 
     def test_methods(self):
         for method in ['get', 'post', 'put', 'delete']:
